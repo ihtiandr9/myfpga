0bb8677973e48f949ceb7daf46e92515ceae9b05
Sergey Ryabchenkov(freevm) <ihtiandr9@yandex.ru>
2025-02-08 19:31:43 +0800

    vga_module add constants & parameters

--- vga_module/vga_module.v
+++ vga_module/vga_module.v
@@ -3,13 +3,20 @@ module vga(input clock, output r, output g, output b, output hs, output vs);
 reg [9:0] x = 10'b0;
 reg [8:0] y = 9'b0;
 
+// ---------------------------------------------------------------------
+// Тайминги для горизонтальной и вертикальной развертки
+//           Visible       Front        Sync        Back       Whole
+parameter hzv =  640, hzf =   16, hzs =   96, hzb =   48, hzw =  800,
+          vtv =  400, vtf =   12, vts =    2, vtb =   35, vtw =  449;
+// ---------------------------------------------------------------------
+
 wire xborder = x == 10'd799;
 wire yborder = y ==  9'd448;
-wire visible = x >= 48 && x < 48+640 && y >= 35 && y <= 35+400;
+wire visible = x >= hzb && x < hzb + hzv && y >= vtb  && y <= vtb + vtv;
 
 // Горизонтальная и вертикальная синхронизация
-assign hs = x  < 48+640+16;
-assign vs = y >= 35+400+12;
+assign hs = x  < hzb + hzv + hzf;
+assign vs = y >= vtb + vtv + vtf;
 
 // Вывод серого экрана
 assign {r, g, b} = visible ? 3'b111 : 3'b000;
