5386a227dca33e13e0f0f412c5ea4a9fb6648fbd
Sergey Ryabchenkov(freevm) <ihtiandr9@yandex.ru>
2025-02-11 20:46:12 +0800

    add uart project

---
+++ uart/uart_testbench.v
@@ -0,0 +1,24 @@
+module test;
+
+  /* Make a rxd that pulses once. */
+  reg rxd = 1;
+  initial begin
+    $dumpfile("test.vcd");
+    $dumpvars(0, test);
+    # 17 rxd = 0;
+    # 868 rxd = 1;
+    # 868 rxd = 0;
+    # (434 * 7 * 2) rxd = 1;
+    # 8000 $finish;
+    $monitor("At time %t, value = %h (%0d)",
+      $time, echo, echo);
+  end
+
+  /* Make a regular pulsing clock. */
+  reg clk = 0;
+  always #1 clk = !clk;
+
+  wire [7:0] echo;
+  uart c1 (echo, clk, rxd);
+
+endmodule // test
